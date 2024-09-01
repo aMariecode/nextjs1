3b304f5bd9cf0088c15895593dac200809f4d8a5
Patricia Marie Mandanas <patricialafaro9@gmail.com>
2024-09-01 22:13:25 +0800

    <updated>

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,10 @@ export default function Home() {
         <article className={styles.post}>
           <h2>Hi there!</h2>
           <p>I am <strong>Patricia Marie Mandanas</strong><br/>I am 33 years old.<br/>And i am proud to be a single mom.<br/> My Hobbies are listening music, playing guitar,cooking and bonding with my kids. <br/> 
-          Favourite Music are in genre of rock and emo. <br/> I am a foodie person.<br/> I am taking up IT because honestly speaking computers were my weakness, when I was in high school since it is just a public school they cannot afford to provide computers for the students. Only private schools could offer computer courses at that time. Secondly, we know nowadays it is the most in-demand in the field.
+          Favourite Music are in genre of rock and emo. <br/> I am a foodie person.<br/> I am taking up IT because honestly speaking computers were my weakness,<br/> when I was in high school since it is just a public school they cannot afford to provide computers for the students. <br/>Only private schools could offer computer courses at that time. Secondly, we know nowadays it is the most in-demand in the field.
 
          <br/> I foreseen myself as a web developer. But as of now I wanted to explore deeply for me to understand and learn more about this field.
-          <br/>  <br/> Keep moving forward  </p> 
+          <br/>  <br/> Keep moving forward! </p> 
         </article>
       </main>
       <footer className={styles.footer}>
